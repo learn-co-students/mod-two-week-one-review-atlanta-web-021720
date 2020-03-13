ef45a236376beb1eb956b1dc7742ac10201a014e
gcampbell391 <gcampbell391@gmail.com>
2020-03-13 17:14:57 -0400

    Done.

--- Rails-Seven-Seas/app/controllers/seas_controller.rb
+++ Rails-Seven-Seas/app/controllers/seas_controller.rb
@@ -1,13 +1,35 @@
 class SeasController < ApplicationController
-  #define your controller actions here
 
+    def index
+        @seas = Sea.all
+    end
 
-  private
-  # In controller actions, use this private method to access sea params from forms.
-  # Example: @sea.update(sea_params)
-  # check the return value of sea_params to see what you're working with!
-  def sea_params
-    params.require(:sea).permit(:name, :temperature, :bio, :mood, :image_url, :favorite_color, :scariest_creature, :has_mermaids)
-  end
+    def show 
+        @sea = Sea.find(params[:id])
+    end
 
+    def new 
+        
+    end
+
+    def create 
+        @sea = Sea.create(name: params["name"], temperature: params["temperature"], bio: params["bio"], image_url: params["image_url"], mood: params["mood"], favorite_color: params["favorite_color"], scariest_creature: params["scariest_creature"], has_mermaids: params["has_mermaids"])
+        redirect_to sea_path(@sea)
+    end
+
+    def edit 
+        @sea = Sea.find(params[:id])
+    end
+
+    def update 
+        @sea = Sea.find(params[:id])
+        @sea.update(name: params[:sea][:name], temperature: params[:sea][:temperature], bio: params[:sea][:bio], image_url: params[:sea][:image_url], mood: params[:sea][:mood], favorite_color: params[:sea][:favorite_color], scariest_creature: params[:sea][:scariest_creature], has_mermaids: params[:sea][:has_mermaids])
+        redirect_to sea_path(@sea)
+    end
+
+    def destroy
+        @sea = Sea.find(params[:id])
+        @sea.destroy
+        redirect_to seas_path
+    end
 end
